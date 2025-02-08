4834c4bcf5aa60e095d6852912c1eac06e8d13cc
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 17:02:55 +0000

    hello.py suntax error corrected

--- week02/hello.py
+++ week02/hello.py
@@ -4,6 +4,6 @@
 
 # Modified 08/02/2025 @ 16:56 to say hello "my name" - aka. Kyra
 # Modified 08/02/2025 @ 16:58 - Syntax error created.
-#Modified 08/02/2025 @ 17:00 - Syntax error corrected.
+# Modified 08/02/2025 @ 17:00 - Syntax error corrected.
 
 print ("Hello Kyra")
