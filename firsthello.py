715513511e7e76402e6fae369ea39b5dcfb0ac90
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 16:56:07 +0000

    week02 folder updated and hello.py added

--- firsthello.py
+++ firsthello.py
@@ -1,1 +1,5 @@
+# Copy of hello world for the general nmy work folder.
+# Will print Hello world upon use.
+# author: Kyra Menai Hamilton
+
 print ("Hello world")
---
+++ week02/hello.py
@@ -0,0 +1,5 @@
+# hello.py
+# This program prints hello "my name" - aka Kyra
+# author: Kyra Menai Hamilton
+
+print ("Hello")
