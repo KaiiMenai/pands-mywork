031fdb215211ca90a2e28c44ba1db82cd6ed27c2
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 17:48:12 +0000

    multiply.py updated for options

--- week02/multiply.py
+++ week02/multiply.py
@@ -5,6 +5,9 @@
 # Initially worksheet says to do "answer = 111 * 555"
 # This did not yield answer - missing key term "print".
 
+answer = 111 *555
+print(answer)
+
 #Following logic of hello.py using print would output correct answer
 
-print (111*555)
+# print (111*555) - shorter version that could be used and still gets correct answer.
