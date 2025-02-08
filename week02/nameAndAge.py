4ff1ce5082bbb2d3d13a7aa97809f14e24168883
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 18:03:40 +0000

    nameAndAge.py created and modified to add greeting

---
+++ week02/nameAndAge.py
@@ -0,0 +1,7 @@
+# nameAndAge.py
+# Prints Hello "name", "nice to meet you", and "age".
+# author: Kyra Menai Hamilton
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+print('Hello ' + name + ',' + '\nNice to meet you.' + '\nYour age is ' + age +'.')
