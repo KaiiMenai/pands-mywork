b93f9f013cf775de1637f118e4af79d01a09eab1
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 18:40:27 +0000

    money.py modified - math operation in placeholder

--- week02/money.py
+++ week02/money.py
@@ -4,10 +4,11 @@
 
 # f-strings - (f for format)
 
-# Modified 08/02/2025 @ 18:30 added in a placeholder {} - it can contain variables.
-# Modified 08/02/2025 @ 18:32 modifier (:) added followed by legal formatting type (i.e. [.2f] - fixed point no. with 2 decimals)
+# Modified 08/02/2025 @ 18:30 - added in a placeholder {} - it can contain variables.
+# Modified 08/02/2025 @ 18:32 - modifier (:) added followed by legal formatting type (i.e. [.2f] - fixed point no. with 2 decimals)
 # Modified 08/02/2025 @ 18:37 - value formatted directly.
+# Modified 08/02/2025 @ 18:39 - math operation in the placeholder.
 
 # price = 60
-txt = f"The price is {95:.2f} euros"
+txt = f"The price is {20*60} euros"
 print(txt)
