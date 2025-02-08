380a7cb8496939963f5ea01d07e16a53c231303c
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 16:59:51 +0000

    hello.py suntax error created

--- week02/hello.py
+++ week02/hello.py
@@ -1,6 +1,7 @@
 # hello.py
 # This program prints hello.
 # Modified 08/02/2025 @ 16:56 to say hello "my name" - aka. Kyra
+# Modified 08/02/2025 @ 16:58 - Syntax error created.
 # author: Kyra Menai Hamilton
 
-print ("Hello Kyra")
+print ("Hello Kyra"
