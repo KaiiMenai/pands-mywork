aab638bb64b0d82404f09e92e2be5559cc797294
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 18:11:32 +0000

    nameAndAge.py modified to have tab instead of new line

--- week02/nameAndAge.py
+++ week02/nameAndAge.py
@@ -2,6 +2,8 @@
 # Prints Hello "name", "nice to meet you", and "age".
 # author: Kyra Menai Hamilton
 
+# Modified to add \t before nice to meet you and before age instead of \n for new line.
+
 name = input("Enter your name:")
 age = input("Enter your age:")
-print('Hello ' + name + ',' + '\nNice to meet you.' + '\nYour age is ' + age +'.')
+print('Hello ' + name + ',' + '\tNice to meet you.' + '\tYour age is ' + age +'.')
