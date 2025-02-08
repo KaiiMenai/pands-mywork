5f54d4c065894a0d0be2513e9f35992ab5435cde
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 18:20:55 +0000

    noequal.py created to find out answer 2==3

---
+++ week02/noequal.py
@@ -0,0 +1,9 @@
+# noequal.py
+# Prints output of 2==3
+# author: Kyra Menai Hamilton
+
+# == in python is a comparator (equality operator) so it is checking whether one is indeed equal to the other.
+
+print(2==3)
+
+#The answer given is false as 2 is not infact equal to 3.
