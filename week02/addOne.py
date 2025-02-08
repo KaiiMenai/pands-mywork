0a9163267d98e85ba2d0cfdc232e264dc9fcd9cd
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 17:57:21 +0000

    addOne.py created

---
+++ week02/addOne.py
@@ -0,0 +1,9 @@
+# addOne.py
+# This program reads a number that is inputted and prints the input +1
+# author: Kyra Menai Hamilton
+
+number = int(input('please enter a number:'))
+newNumber = number + 1
+print (f'{number} plus one is {newNumber}')
+
+# int (function) converts number input into an integer so that python sees it as a number not just as text input.
