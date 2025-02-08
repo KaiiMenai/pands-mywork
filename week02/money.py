70b859f55beb130227e38837628fceba057079c9
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 18:38:26 +0000

    money.py modified - direct value formatted

--- week02/money.py
+++ week02/money.py
@@ -6,7 +6,8 @@
 
 # Modified 08/02/2025 @ 18:30 added in a placeholder {} - it can contain variables.
 # Modified 08/02/2025 @ 18:32 modifier (:) added followed by legal formatting type (i.e. [.2f] - fixed point no. with 2 decimals)
+# Modified 08/02/2025 @ 18:37 - value formatted directly.
 
-price = 60
-txt = f"The price is {price:.2f} euros"
+# price = 60
+txt = f"The price is {95:.2f} euros"
 print(txt)
