4b95bcb07cbfee6482604547f8b338bd59ea9a65
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 17:43:51 +0000

    hello2.py modified to add nice to meet you

---
+++ week02/hello2.py
@@ -0,0 +1,10 @@
+# hello2.py
+# Prints Hello "name"
+# author: Kyra Menai Hamilton
+
+# Modified 09/02/2025 @ 17:27 - added greeting line "nice to meet you" using \nNice to meet you to put it on a new name following input.
+
+name = input("Enter your name:")
+print('Hello ' + name + '\nNice to meet you')
+
+# Alternative method of input - print(f'Hello {name}\nNice to meet you')
