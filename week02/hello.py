96e69c395cee20d329ba4e4a7a7f5ad3317b9bbe
unknown <kyra.menai.hamilton@outlook.com>
2025-02-08 16:58:02 +0000

    week02 folder updated and hello.py modified to add my name

--- week02/hello.py
+++ week02/hello.py
@@ -1,5 +1,6 @@
 # hello.py
-# This program prints hello "my name" - aka Kyra
+# This program prints hello.
+# Modified 08/02/2025 @ 16:56 to say hello "my name" - aka. Kyra
 # author: Kyra Menai Hamilton
 
-print ("Hello")
+print ("Hello Kyra")
