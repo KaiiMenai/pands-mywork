369d498f2d2a1842f2c52bcd724d58026d096012
unknown <kyra.menai.hamilton@outlook.com>
2025-02-11 21:17:29 +0000

    randomGenerator.py created

---
+++ week03/randomGenerator.py
@@ -0,0 +1,15 @@
+# randomGenerator.py
+# This program will print out a random number between 1 and 10.
+# author: Kyra Menai Hamilton
+
+import random
+print("Please state a start and end for range.")
+start = int(input("Enter start number: "))
+stop = int(input("Enter end number: "))
+
+number = random.randint(start, stop)
+print("Here is a random number, {}.".format(number))
+
+# Reference:
+# Using randint() function: https://www.geeksforgeeks.org/python-generate-random-numbers-within-a-given-range-and-store-in-a-list/
+# Can also state if the output value needs to be above/below a specific value.
