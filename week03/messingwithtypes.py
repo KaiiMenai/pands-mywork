5f156eeb8792af7e84b5c5b038023c5c6cd9e7d2
unknown <kyra.menai.hamilton@outlook.com>
2025-02-11 20:09:12 +0000

    messingwithtypes.py created

---
+++ week03/messingwithtypes.py
@@ -0,0 +1,21 @@
+# messingwithvariabletypes.py
+# Messing with different types of variables and the functions that can be used with them.#
+# author: Kyra Menai Hamilton
+
+# Saying answer = 12 isn't enough, this is just a variable with a value, it hasn't been told to do anything. 
+# So it won't show anything when run. Need to tell it to do something i.e. print(X).
+
+answer = 12
+print (f"The answer is {answer}.")
+print(f"Type of answer is {type(answer)}")
+
+# NOTES
+# Variable names are case sensitive. If 'Answer' is typed instead of 'answer', it will give an error and say something like "answer is not defined".
+# DO NOT add different variables that only differ by the capitalisation of a letter (in large datasets it'll be a nightmare).
+# Make the code easy to read. Include explanations.
+
+# Recommended that all variables start with a small letter.
+# Python naming convention for particularly long variable names will include _ between words. In different programming languages this will be different. 
+# Make sure that all variables have proper names, it will make it easier to read the code and understand what it does.
+# MUST start variable names with a letter, NOT a number. Numbers can be in the middle of a variable name just not at the beginning.
+# print(type(X)) will yield the type of variable that X is. Whether that is variable (e.g. answer as integer) as above.
