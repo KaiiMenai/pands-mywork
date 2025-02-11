0b8deb248bbeef38c92e7fb747eeadb84a104912
unknown <kyra.menai.hamilton@outlook.com>
2025-02-11 21:39:37 +0000

    burritos.py created and modified

---
+++ week03/burritos.py
@@ -0,0 +1,22 @@
+# burritos.py
+# This program will print the number of burritos a person has eaten. The person here is "I".
+# author: Kyra Menai Hamilton
+
+# The initial input provided doesn't work.
+
+# message = 'I have eaten ' + 99 + ' burritos.'
+# print (message)
+
+# This is due to the initial statement - 'I have eaten ' + 99 + ' burritos.'
+# To modify this so that it works.
+
+number = int(99)
+print("I have eaten {} burritos.".format(number))
+
+# Modified: 11/-2/2025 @ 21:38 - Make it so that someone can input their own value/number of burritos.
+
+# To be fancy about it and get someone to input their own value
+
+# print("How many burritos have you eaten?")
+# number = int(input("Enter number: "))
+# print("You ate {} burritos.".format(number))
