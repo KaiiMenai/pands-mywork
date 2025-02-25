089a374aa9d3c0ca7d03491c602ad70f2e4ba628
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:53:20 +0000

    pass statement and condition explored

---
+++ week04/passstatement.py
@@ -0,0 +1,37 @@
+# passstatement.py
+# This program demonstrates the use of the "pass" statement in Python.
+# author: Kyra Menai Hamilton
+
+# The "pass" statement is used as a placeholder for future code.
+# The "pass" statement is a null operation that does nothing.
+# The "pass" statement is used when a statement is required syntactically but you do not want any command or code to execute.
+# The "pass" statement is used to prevent an error when no action is required.
+# The "pass" statement is used to create empty code blocks.
+# The "pass" statement is used to create empty functions or classes.
+# The "pass" statement is used to create empty loops.
+# The "pass" statement is used to create empty if statements.
+# The "pass" statement is used to create empty elif statements.
+# The "pass" statement is used to create empty else statements.
+
+# In this example, we will use the "pass" statement to create an empty code block.
+# We will create an empty function called myfunction.
+# We will use the "pass" statement to create an empty code block inside the function.
+
+def myfunction():
+    pass
+
+# Output:
+    # No output will be displayed as the function is empty.
+
+# Even if there is an input, the function will not return anything as it is empty.
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+# Output:
+    # No output will be displayed as the code block is empty.
+
+# END
