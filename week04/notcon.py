973d91390f47a072d2493b015084b6b61e7419bd
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:50:53 +0000

    not condition exploration

---
+++ week04/notcon.py
@@ -0,0 +1,23 @@
+# notcon.py
+# This program demonstrates the use of the "not" condition in Python.
+# author: Kyra Menai Hamilton
+
+# The "not" condition is used to reverse the logical state of a conditional statement.
+# The "not" condition is an example of a logical operator.
+# The "not" condition is used to reverse the result of a comparison.
+# If a condition is true, the "not" condition will return false.
+# If a condition is false, the "not" condition will return true.
+
+# In this example, we will use the "not" condition to reverse the result of a comparison.
+# We will check if a is not greater than b.
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+# Output:
+    # a is NOT greater than b
+    # This will print "a is NOT greater than b" as the condition is false.
+
+# END
