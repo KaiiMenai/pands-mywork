281f444a079bb694198c55b3c8e202f7b26528e1
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:39:08 +0000

    short if practice

---
+++ week04/shortif.py
@@ -0,0 +1,16 @@
+# shortif.py
+# This program demonstrates the use of a short if statement in Python.
+# author: Kyra Menai Hamilton
+
+# The short if statement is a one-liner that allows you to execute a statement if a condition is true.
+# It is used when you have only one statement to execute.
+
+a = 200
+b = 33
+if a > b: print("a is greater than b")
+
+# Output:
+    # a is greater than b
+    # This will print "a is greater than b" as 200 is greater than 33.
+
+# END
