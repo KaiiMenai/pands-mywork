e13b62d82113cd751270b64992e3d12a26222019
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:46:38 +0000

    and condition exploration

---
+++ week04/andcondition.py
@@ -0,0 +1,25 @@
+# andcondition.py
+# This program demonstrates the use of the "and" condition in Python.
+# author: Kyra Menai Hamilton
+
+# The "and" condition is used to combine two conditional statements.
+# The "and" condition is only true if both the conditions are true.
+# The "and" condition is an example of a logical operator.
+# The "and" condition is used to check if both conditions are true.
+# If both conditions are true, the "and" condition will return true.
+
+# In this example, we will use the "and" condition to check if both conditions are true.
+# We will check if a is greater than b and if c is greater than a.
+# If both conditions are true, the "and" condition will return true.
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+# Output:
+    # Both conditions are True
+    # This will print "Both conditions are True" as both conditions are true.
+
+# END
