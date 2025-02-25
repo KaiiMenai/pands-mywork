a10c16030ebcec906fdac665a5ca3300fc041ea3
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:48:58 +0000

    or condition exploration

---
+++ week04/orcondition.py
@@ -0,0 +1,21 @@
+# orcondition.py
+# This program demonstrates the use of the "or" condition in Python.
+# author: Kyra Menai Hamilton
+
+# The "or" condition is used to combine two conditional statements. 
+# The "or" condition is true if one of the conditions is true.
+# The "or" condition is an example of a logical operator.
+# The "or" condition is used to check if at least one condition is true.
+# If one of the conditions is true, the "or" condition will return true.
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+# Output:
+    # At least one of the conditions is True
+    # This will print "At least one of the conditions is True" as one of the conditions is true.
+
+# END
