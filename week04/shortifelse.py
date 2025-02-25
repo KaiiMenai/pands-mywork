6ab03dd4f3be0bc9b9cef0b18def76bc667543fb
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-02-25 18:42:42 +0000

    added terminology terminology

--- week04/shortifelse.py
+++ week04/shortifelse.py
@@ -14,6 +14,7 @@ print("A") if a > b else print("B")
     # This will print "B" as a is not greater than b.
 
 # With the short if-else statement, you can execute multiple else statements on the same line.
+# This technique is known as Ternary Operators, or Conditional Expressions.
 
 a = 330
 b = 330
