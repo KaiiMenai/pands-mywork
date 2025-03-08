0335e477c37a9e6a989eb2b28500bcd400036cf6
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-03-08 12:34:48 +0000

    evens.py lab 4.2 loops

---
+++ week04/evens.py
@@ -0,0 +1,27 @@
+# evens.py
+# This program will use a while loop to print all the even numbers from 2 to 100.
+# author: Kyra Menai Hamilton
+
+numberto = 100
+evennum = 2
+
+# The pattern remains the same:
+    # Initialise
+    # Check
+    # Change
+
+while evennum <= numberto:  # While the even number is less than or equal to 100
+    print (evennum)         # Print out the even number
+    evennum += 2            # The previous even number + 2.
+
+# Output:
+    # 2
+    # 4
+    # 6
+    # ...
+    # 96
+    # 98
+    # 100
+# This program has printed out every number between 2 and 100 (inclusive).
+
+# END
