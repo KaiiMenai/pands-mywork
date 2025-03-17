f3318c2174b075f8092bbd6589745256a97a3566
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-03-17 16:58:14 +0000

    funvtion variables

---
+++ week06/varifunction.py
@@ -0,0 +1,17 @@
+# varifunvtion.py
+# Exploring that variables can be a type of function.
+# author: Kyra Menai Hamilton
+
+
+# Defining a new function that will return a function.
+def fun1():
+    print("this is fun1")
+def fun2():
+    print("this is fun2")
+
+whichFun = fun1
+whichFun()
+whichFun= fun2
+whichFun()
+
+# END
