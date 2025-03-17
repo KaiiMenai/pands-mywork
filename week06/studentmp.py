4ab10f9b608542eee46e1d7903fff4128b629c4e
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-03-17 16:14:15 +0000

    Bare bones of prog for students

--- week06/studentmp.py
+++ week06/studentmp.py
@@ -3,56 +3,46 @@
 # The program then prints this data out, sorted by student name.
 # author: Kyra Menai Hamilton
 
-import pandas as pd
-import numpy as np
-
-
 # Ask the user what they would like to do.
 
-print("What would you like to do? \nPlease select from the following options: ")
-print("\t(a) Add new student details")
-print("\t(v) View student details")
-print("\t(q) Quit)")
-choice = input("Type one letter (a/v/q): ").strip()
+def display_menu():
+    print("What would you like to do? \nPlease select from the following options: ")
+    print("\t(a) Add new student details")
+    print("\t(v) View student details")
+    print("\t(q) Quit)")
+    choice = input("Type one letter (a/v/q): ").strip()
 
+    return choice
+# Test that this actually works.
 
-# Initialize an empty list to store all students' details
-students = []
+choice = display_menu()
+print(f"You chose {choice}.")
 
-# Continue to read in student details until a blank student ID is entered
-while True:
-    # Read the student's name (or ID)
-    identity = input("Please enter the student's name (or leave empty and press Enter to finish): ")
-    if not identity.strip():  # Break if the student name/ID is empty or whitespace
-        break
+# Here we have defined the choices and also reiterate what choice the user has made.
 
-    # Create a dictionary for the current student
-    student = {
-        "name": identity,
-        "modules": []
-    }
+# Next we will add the ability to add a student.
+# A function will be created to do this, and it will need to be called when the user selects 'a'.
 
-    # Continue to read in course names and grades for this student until a blank course name is entered
-    while True:
-        courseid = input("Please enter the course name (or leave empty and press Enter to finish): ")
-        if not courseid.strip():  # Break if the course name is empty or whitespace
-            break
+def do_add():
+    print("In adding.")         # This is just a placeholder for now.
+    
+# Next we will add the ability to view a student.
+def do_view():
+    print("In viewing.")        # Again, just a placeholder, will be elaborated later.
+    
+# Now we will add the ability to quit the program.
+def do_quit():
+    print("Quitting.")
 
-        # Read the grade for the entered course
-        mark = input(f"Please enter the student's grade for {courseid}: ")
+# Now to add the details.
+# The main program loop is as follows:
 
-        # Add the module details to the student's modules list
-        student["modules"].append({
-            "course": courseid,
-            "mark": mark
-        })
-
-    # Add the completed student dictionary to the list of students
-    students.append(student)
-
-# Print out all students' details
-print("\nAll Students:")
-for student in students:
-    print("Student: {}".format(student["name"]))
-    for module in student["modules"]:
-        print("\t{} \t: {}".format(module["course"], module["mark"]))
+choice = display_menu()
+while choice != 'q':            # While choice doesn't equal 'q', the program will continue to run.
+    if choice == 'a':           # If the choice is 'a', the program will run the do_add function.
+        do_add()
+    elif choice == 'v':         # If the choice is 'v', the program will run the do_view function.
+        do_view()
+    elif choice != 'q':         # If the choice is not 'q', the program will print an error message.
+        print("\n\nPlease select either a, v or q.")
+    choice = display_menu()
